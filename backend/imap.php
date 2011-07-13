e2a0c56654eb7beaaa1ff705832a3b2ec00420c7
Matthieu Patou <mat@matws.net>
2011-07-13 18:20:00 +0400

    For inline forward, don't forget to attach the body inline if it hadn't been done before

--- backend/imap.php
+++ backend/imap.php
@@ -460,6 +460,12 @@ class BackendIMAP extends BackendDiff {
                             }
                         }
                     }
+                    if (!$attached) {
+                        // We need to attach the curent body as an attachement as it hasn't been done
+                        // before and we registered the message as a multipart.
+                        $attached = true;
+                        $body = $this->enc_multipart($att_boundary, $body, $forward_h_ct, $forward_h_cte);
+                    }
                     $body .= "--$att_boundary--\n\n";
                 }
 
